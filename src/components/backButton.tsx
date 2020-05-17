632e1dd34b7d9e485c45d5f302db5f67f3e720a3
nikolarusakov <nikola.rusakov@rusakov.cz>
2020-05-18 01:35:58 +0200

    fix(page/detail): remove back button bg

--- src/components/backButton.tsx
+++ src/components/backButton.tsx
@@ -5,7 +5,9 @@ import Router from 'next/router';
 import React from 'react';
 
 const BackButton: React.FC = ({ children }) => (
-  <button css={{ border: 'none', '&:focus': { outline: 'thick double #32a1ce' } }} onClick={() => Router.back()}>
+  <button
+    css={{ border: 'none', background: 'transparent', '&:focus': { outline: 'thick double #32a1ce' } }}
+    onClick={() => Router.back()}>
     {children}
   </button>
 );
