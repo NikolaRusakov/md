5c5d00e6e0cc37a635a7e47af674eb707b65f9cf
nikolarusakov <nikola.rusakov@rusakov.cz>
2020-05-18 01:53:15 +0200

    feat(page/landing): add landing

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,33 @@
-import Link from "next/link";
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import Link from 'next/link';
 
 export default function IndexPage() {
   return (
-    <div>
-      Hello World.{" "}
-      <Link href="/about">
-        <a>About</a>
+    <div
+      css={{
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Link href="/browse">
+        <a css={theme => ({ fontSize: theme.sizes['24'] })}>
+          <h1
+            css={{
+              cursor: 'pointer',
+              textAlign: 'center',
+              textDecoration: 'underline',
+              textDecorationColor: '#0000',
+              transition: 'all 0.2s ease-in-out',
+              '&:hover': {
+                textDecorationColor: '#000',
+              },
+            }}>
+            Browse your favorite content
+          </h1>
+        </a>
       </Link>
     </div>
   );
